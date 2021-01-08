46022b68a0a78603e5864466e3c5662f6f79c441
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-01-08 15:15:31 +0900

    learning fetching data & sending data with native MongoClient

--- mongo.js
+++ mongo.js
@@ -4,9 +4,52 @@ const DATABASE_PASSWORD = require('./secret');
 
 const url = `mongodb+srv://shotaro:${DATABASE_PASSWORD}@cluster0.omrq7.mongodb.net/products_test?retryWrites=true&w=majority`;
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  //targeting specific dbServer
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  try {
+    //connecting dbServer
+    await client.connect();
+    //accessing a specific database
+    const db = client.db(); //access 'products_test' db
+    //create a Document on collection(products)
+    const result = await db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    // console.log(error);
+    return res.json({ message: 'Could not store data.' });
+  }
+  //このままだとdbとのconnectionが維持されるのでdbとのconnectionをcloseする
+  client.close();
 
-const getProducts = async (req, res, next) => {};
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  let products;
+  try {
+    //connecting dbServer
+    await client.connect();
+    //accessing a specific database
+    const db = client.db();
+    //fetch a Document on collection(products)
+    products = await db.collection('products').find().toArray();
+  } catch (error) {
+    return res.json({ message: 'Could not retrieve products.' });
+  }
+  client.close();
+  res.json(products);
+};
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
